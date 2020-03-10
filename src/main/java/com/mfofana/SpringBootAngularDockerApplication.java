56469e32cc36a77e9309d8d69d90c2a6cb624a2d
Mohamed Fofana <manoufof@hotmail.com>
2020-03-10 22:03:47 +0100

    Adding sources

---
+++ src/main/java/com/mfofana/SpringBootAngularDockerApplication.java
@@ -0,0 +1,13 @@
+package com.mfofana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfofana/SpringBootAngularDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfofana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
